dc8df756ed37e0205e82b959af372d80b0167589
Levko Kravets <levko.ne@gmail.com>
2017-03-06 23:10:27 +0200

    openspending/subsidystories.eu#34 Do not show some blocks if there are no data for selected period

--- app/scripts/application/components/visualizations/index.js
+++ app/scripts/application/components/visualizations/index.js
@@ -1,20 +1,45 @@
 'use strict';
 
+var _ = require('lodash');
+var Vuex = require('vuex');
 var subsidyStories = require('../../services/subsidy-stories');
 
 module.exports = {
   template: require('./template.html'),
   data: function() {
     return {
+      isLoaded: false,
+      isAvailable: false,
       visualizations: subsidyStories.availableVisualizations
     };
   },
-  methods: {
-    getVisualizationUrl: function(visualization) {
-      var countryCode = this.$store.state.countryCode;
-      var period = this.$store.state.period;
-      return subsidyStories.getVisualizationUrl(visualization,
-        countryCode, period);
+  computed: Vuex.mapState([
+    'period',
+    'countryCode'
+  ]),
+  watch: {
+    period: function() {
+      this.refresh();
     }
+  },
+  methods: _.extend({
+    getVisualizationUrl: function(visualization) {
+      return subsidyStories.getVisualizationUrl(visualization,
+        this.countryCode, this.period);
+    },
+    refresh: function() {
+      var that = this;
+      that.$data.isLoaded = false;
+      that.$data.isAvailable = false;
+      that.getTotalSubsidies().then(function(value) {
+        that.$data.isAvailable = value > 0;
+        that.$data.isLoaded = true;
+      });
+    }
+  }, Vuex.mapActions([
+    'getTotalSubsidies'
+  ])),
+  created: function() {
+    this.refresh();
   }
 };
